5b29c53dd9eb837b8d19239cd1e179c09f8a2fc5
T N <truong1320vv@gmail.com>
2021-10-31 20:53:44 +0700

    deploy to github

--- src/util/config.js
+++ src/util/config.js
@@ -1,9 +1,9 @@
 //local
-export const API_AUTH_URL = "http://localhost:3000/api/auth/";
-export const API_CLASS_URL = "http://localhost:3000/api/class/";
+// export const API_AUTH_URL = "http://localhost:3000/api/auth/";
+// export const API_CLASS_URL = "http://localhost:3000/api/class/";
 //deploy
-// export const API_AUTH_URL = "https://react-classroom-api.herokuapp.com/api/auth/";
-// export const API_CLASS_URL = "https://react-classroom-api.herokuapp.com/api/class/";
+export const API_AUTH_URL = "https://react-classroom-api.herokuapp.com/api/auth/";
+export const API_CLASS_URL = "https://react-classroom-api.herokuapp.com/api/class/";
 export const COOKIE_FIELD_NAME = "myCat";
 
 //strictly set url to match hosting page
