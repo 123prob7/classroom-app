bf0f75e8ce5879daceae57caabafcb5dce03c785
T N <truong1320vv@gmail.com>
2021-10-31 21:44:43 +0700

    fix image's url

--- src/util/config.js
+++ src/util/config.js
@@ -5,4 +5,4 @@
 export const API_AUTH_URL = "https://react-classroom-api.herokuapp.com/api/auth/";
 export const API_CLASS_URL = "https://react-classroom-api.herokuapp.com/api/class/";
 export const COOKIE_FIELD_NAME = "myCat";
-export const CLASS_BANNER_IMAGE = "/static/images/jerry.jpg";
+export const CLASS_BANNER_IMAGE = "/classroom-app/static/images/jerry.jpg";
